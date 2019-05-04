5668f2e4e51868d1f1ab5ed5f85ac5e71a16ee59
Artem Vasiliev <amcour@gmail.com>
2019-05-04 18:46:16 +0300

    few fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo systemctl restart nginx 
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo systemctl restart gunicorn
-sudo systemctl start mysql
+#sudo systemctl restart gunicorn
+#sudo systemctl start mysql
