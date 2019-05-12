d69032bc012e30e0c1836b714e42f0f7a1b83dc3
Artem Vasiliev <amcour@gmail.com>
2019-05-12 18:49:18 +0300

    unlink 'default' in nginx sites-enabled

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
